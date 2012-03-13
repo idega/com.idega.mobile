d12e9637f04ecbd605c998e3bdfa84d1e81a062e
Valdas Žemaitis <valdas@idega.com>
2012-03-13 21:48:01 +0200

    Initial version

---
+++ src/java/com/idega/mobile/IWBundleStarter.java
@@ -0,0 +1,18 @@
+package com.idega.mobile;
+
+import com.idega.idegaweb.IWBundle;
+import com.idega.idegaweb.IWBundleStartable;
+
+public class IWBundleStarter implements IWBundleStartable {
+
+	public void start(IWBundle starterBundle) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void stop(IWBundle starterBundle) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/java/com/idega/mobile/MobileConstants.java
@@ -0,0 +1,9 @@
+package com.idega.mobile;
+
+public class MobileConstants {
+
+	private MobileConstants() {}
+
+	public static final String IW_BUNDLE_IDENTIFIER = "com.idega.mobile";
+
+}
